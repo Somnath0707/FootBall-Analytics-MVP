8b33b131124fc836e304ae32411020a1706fc32a
Somnath Ghorpade <somnathghorpade@Somnaths-MacBook-Air.local>
2025-11-14 11:42:09 +0530

    Adeed Ball InterPolarisation and Team Ball Control

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
 import pickle
+import pandas as pd
 import os
 import cv2
 import sys
@@ -14,6 +15,18 @@ class Tracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path) 
         self.tracker = sv.ByteTrack()
+    
+    def interpolate_ball_positions(self,ball_positions):
+        ball_positions = [x.get(1,{}).get('bbox',[]) for x in ball_positions]
+        df_ball_positions = pd.DataFrame(ball_positions,columns=['x1','y1','x2','y2'])
+
+        # Interpolate missing values
+        df_ball_positions = df_ball_positions.interpolate()
+        df_ball_positions = df_ball_positions.bfill()
+
+        ball_positions = [{1: {"bbox":x}} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
 
     def detect_frames(self, frames):
         batch_size=20 
@@ -131,21 +144,40 @@ class Tracker:
         return frame
     
 
-    def draw_traingle(self, frame, bbox, color):
-        y = int(bbox[1])
-        x , _ = get_center_of_bbox(bbox)
+    def draw_traingle(self,frame,bbox,color):
+        y= int(bbox[1])
+        x,_ = get_center_of_bbox(bbox)
 
         triangle_points = np.array([
             [x,y],
-            [x-10, y-20],
-            [x+10, y-20]
-            ])
-        cv2.drawContours(frame,[triangle_points],0,color, cv2.FILLED)
-        cv2.drawContours(frame,[triangle_points],0,(0,0,0),2)
+            [x-10,y-20],
+            [x+10,y-20],
+        ])
+        cv2.drawContours(frame, [triangle_points],0,color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points],0,(0,0,0), 2)
 
         return frame
 
-    def draw_annotations(self, video_frames,tracks):
+
+    def draw_team_ball_control(self,frame,frame_num,team_ball_control):
+        # Draw a semi-transparent rectaggle 
+        overlay = frame.copy()
+        cv2.rectangle(overlay, (1350, 850), (1900,970), (255,255,255), -1 )
+        alpha = 0.4
+        cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+
+        team_ball_control_till_frame = team_ball_control[:frame_num+1]
+        # Get the number of time each team had ball control
+        team_1_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==1].shape[0]
+        team_2_num_frames = team_ball_control_till_frame[team_ball_control_till_frame==2].shape[0]
+        team_1 = team_1_num_frames/(team_1_num_frames+team_2_num_frames)
+        team_2 = team_2_num_frames/(team_1_num_frames+team_2_num_frames)
+
+        cv2.putText(frame, f"Team 1 Ball Control: {team_1*100:.2f}%",(1400,900), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+        cv2.putText(frame, f"Team 2 Ball Control: {team_2*100:.2f}%",(1400,950), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+
+        return frame
+    def draw_annotations(self, video_frames,tracks,team_ball_control):
         # 4. FIX: Corrected typo 'vieo' to 'video'
         output_video_frames = []
         for frame_num, frame in enumerate(video_frames):
@@ -161,6 +193,9 @@ class Tracker:
                 color = player.get("team_color",(0,0,255))
                 frame = self.draw_ellipse(frame, player["bbox"],color,track_id)
 
+                if player.get("has_ball",False):
+                    frame = self.draw_traingle(frame, player["bbox"],(0,0,255))
+
             # Draw Refreee
             # Draw Refreee
             for _ , refree in referee_dict.items():
@@ -168,6 +203,10 @@ class Tracker:
             #Draw Ball
             for track_id , ball in ball_dict.items():
                 frame = self.draw_traingle(frame, ball['bbox'],(0,255,0))
+        
+
+            # Draw team ball control 
+            frame = self.draw_team_ball_control(frame, frame_num, team_ball_control)
 
             # 5. FIX: Indentation - This append should be *outside* the inner loop
             output_video_frames.append(frame)
